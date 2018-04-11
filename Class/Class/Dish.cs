d271138bfeab8db03cc3e00b7d9010bcb7b42703
thelittlewozniak <nathan.pire@gmail.com>
2018-04-12 00:54:38 +0200

    Creating Class: Catalog               -> fully implemented Dish                     -> fully implemented Neighbour          -> not fully implemented Order                   -> fully implemented Schedule             -> fully implemented User                     -> not fully implemented BddConnection -> not created BddRequest        -> not created

---
+++ Class/Class/Dish.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Dish
+    {
+        private List<string[]> listComments;
+        private List<string> listIngredients;
+        private string thema;
+        private string type;
+        private float price;
+        public List<string[]> GetComments => listComments;
+        public List<string> GetIngredient => listIngredients;
+        public string GetThema => thema;
+        public string GetTypeDish => type;
+        public float GetPrice => price;
+        public Dish(List<string[]> listComments,List<string> listIngredients,string thema,string type, float price)
+        {
+            this.listComments =listComments;
+            this.listIngredients = listIngredients;
+            this.thema = thema;
+            this.type = type;
+            this.price = price;
+        }
+        public void AddDateToSchedule(DateTime date, Schedule schedule) => schedule.AddAvailableDate(date);
+    }
+}
---
+++ Class/Class/Neighbour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Neighbour:User
+    {
+        public Neighbour(string username,Catalog catalog,Schedule schedule):base(username,catalog,schedule)
+        {
+
+        }
+    }
+}
