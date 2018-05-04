ec1255f05ccf1140d439949754298f0de01ffcf4
Liraxiau <38355470+Liraxiau@users.noreply.github.com>
2018-05-04 13:00:26 +0200

    formulaire recette
    amélioration formulaire recette

--- onessaye/onessaye/Controllers/RecipeController.cs
+++ onessaye/onessaye/Controllers/RecipeController.cs
@@ -18,12 +18,19 @@ namespace onessaye.Controllers
         [HttpGet]
         public ActionResult DisplayRecipeForm(string NbIngredients="3")
         {
-            ViewBag.NbIngredients = int.Parse(NbIngredients);
+            int nbi = Convert.ToInt32(NbIngredients);
+            if (nbi < 3) nbi = 3; //Can't have less than 3 ingredients
+            ViewBag.NbIngredients = nbi;
             return View("RecipeForm");
         }
-        [HttpGet]
-        public ActionResult CheckRecipe()
+        [HttpPost]
+        public ActionResult CheckRecipe(string name, string[] tabIngredients, string[] tabAmounts, string[]tabPrices)
         {
+            ViewBag.Name = name;
+            ViewBag.ListIngredients = tabIngredients;
+            ViewBag.ListAmounts = tabAmounts;
+            ViewBag.ListPrices = tabPrices;
+            ViewBag.NbIngredients = tabIngredients.Length;
             return View();
         }
     }
