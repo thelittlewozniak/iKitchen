a607c07f045de34811b4317b9bf0bdd444b1e376
Liraxiau <38355470+Liraxiau@users.noreply.github.com>
2018-05-21 18:46:44 +0200

    mise à jour ingrédients

--- onessaye/onessaye/Models/DAL/RecipeDAL.cs
+++ onessaye/onessaye/Models/DAL/RecipeDAL.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Data.Entity;
 using onessaye.Models.POCO;
 using System.Data.Entity;
 //Bisconti Flavian
@@ -24,11 +23,7 @@ namespace onessaye.Models.DAL
         }
         public Recipe GetRecipe(int id)
         {
-<<<<<<< HEAD
-            return dbc.DbRecipe.Include(r=>r.ListIngredients).SingleOrDefault(r => r.Id == id);
-=======
             return dbc.DbRecipe.Include(c=>c.Schedules).Include(c=>c.Schedules.ListDate).SingleOrDefault(r => r.Id == id);
->>>>>>> ab72d0cc25d171b14f6f9ea69458ccc9f09009ff
         }
     }
 }
